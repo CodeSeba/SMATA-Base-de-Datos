75ae0374bcca8d359d8745d748aa912ace5720fd
CodeSeba <31450548+CodeSeba@users.noreply.github.com>
2017-08-29 15:27:26 -0300

    Delete query-Ago22-ejercicio05.sql

--- Clase2/query-Ago22-ejercicio05.sql
+++
@@ -1,7 +0,0 @@
-USE libreria
-
--- Obterner Titulo, Stock, Precio de los libros con Stock > 5 ordenado por titulo descendent
-SELECT Titulo,Stock,Precio
-	FROM libros
-    WHERE Stock > 5
-    ORDER BY Titulo DESC;
