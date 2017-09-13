39ac75d8c825ce733d5ef28e4c4b7cb4b9b1c426
CodeSeba <seba84x@gmail.com>
2017-09-12 22:16:34 -0300

    Clase 05

--- Clase05/create-Sep05-ejercicio01.sql
+++ Clase05/create-Sep05-ejercicio01.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE cines;
+CREATE DATABASE cine;
 
-USE cines;
+USE cine;
 
 CREATE TABLE salas
 	(
@@ -14,7 +14,50 @@ CREATE TABLE peliculas
 	(
 		codigo int AUTO_INCREMENT,
 		nombre varchar(100),
-		calificacion int,
-		edad int,
+		calificacion int, -- Calificaicon por Edad
 		PRIMARY KEY (codigo)
 	);
+
+CREATE TABLE calificaciones
+	(
+		codigo int,
+		nombre varchar(100),
+		PRIMARY KEY (codigo)
+	);
+
+INSERT INTO salas(nombre,pelicula) VALUES
+	('Sarmiento',NULL),
+	('Bolivar',3),
+	('Evita',2),
+	('Pueyrredon',NULL),
+	('Viamonte',8);
+
+INSERT INTO calificaciones(codigo,nombre) VALUES
+	(1,'ATP'),
+	(2,'+13'),
+	(3,'+16'),
+	(4,'+18');
+
+INSERT INTO peliculas(nombre,calificacion) VALUES
+	('Avatar',1),
+	('Lawrence of Arabia',NULL),
+	('Donnie Darko',NULL),
+	('Aliens',4),
+	('Vertigo',NULL),
+	('The Innocents',NULL),
+	('Slumdog Millionaire',NULL),
+	('Annie Hall',NULL),
+	('Good Will Hunting',NULL),
+	('American Beauty',4),
+	('Titanic',3),
+	('Beyond the Sea',NULL),
+	('Trainspotting',3),
+	('Princess Mononoke',1),
+	('The Usual Suspects',NULL),
+	('Blade Runner',NULL),
+	('Braveheart',2),
+	('Chinatown',4);
+
+SELECT * FROM salas;
+
+SELECT * FROM peliculas;
